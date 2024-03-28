134f206af611de9e332790a44e92d960f5de3731
Mads Nylund <madsnyl@gmail.com>
2024-03-28 14:34:36 +0100

    finished bulk adding of registrations

---
+++ registrations/bulkAdd/settings.py
@@ -0,0 +1,10 @@
+
+
+PROD_USER_ENDPOINT = "https://api.tihlde.org/users/"
+PROD_EVENT_ENDPOINT = "https://api.tihlde.org/events/"
+PROD_ADD_REGISTRATION_ENDPOINT = "/registrations/add/"
+
+DEV_USER_ENDPOINT = "https://api-dev.tihlde.org/users/"
+DEV_EVENT_ENDPOINT = "https://api-dev.tihlde.org/events/"
+DEV_ADD_REGISTRATION_ENDPOINT = "/registrations/add/"
+
